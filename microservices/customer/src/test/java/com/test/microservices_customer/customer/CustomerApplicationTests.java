c60dc1759a21869352796816c59fe8433494dce4
gianmarcoCossioArmijos <117977501+gianmarcoCossioArmijos@users.noreply.github.com>
2025-08-25 03:11:35 -0500

    configiurations for microservices, docker and eureka

---
+++ microservices/customer/src/test/java/com/test/microservices_customer/customer/CustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.microservices_customer.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
